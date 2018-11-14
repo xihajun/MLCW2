19258d453a85feeacf6c3f2533d92623263f79b7
xihajun <www.junfan.com@gmail.com>
2018-11-14 10:14:33 +0000

    Update Simple.m

--- Simple.m
+++ Simple.m
@@ -1,3 +1,9 @@
+imdata = imread('https://images0.cnblogs.com/blog/614265/201408/241708117531878.png');
+I=im2bw(imdata);
+%I = double(I) / 255;
+r = normrnd(0,1,size(I));
+J = I+r;
+imshow(J)
 for i = 1 : 582
     for j =1:1087
         if dist(J(i,j),1)<dist(J(i,j),0)
