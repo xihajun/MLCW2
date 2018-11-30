e61e8cc937e1b451e69272b9029fa25c5150a0f9
xihajun <www.junfan.com@gmail.com>
2018-11-30 00:46:07 +0000

    Add files via upload

--- cw2.m
+++ cw2.m
@@ -6,17 +6,45 @@ J=I+0;
 J = I+r;
 imshow(J)
 
+parfor i = 1 : 582
+    for j =1:1087
+        if dist(J(i,j),1)<dist(J(i,j),0)
+            J(i,j)=1;
+        else
+            J(i,j)=0;
+        end
+    end
+end
 J(find(J==0))=-1;
 figure
 imshow(J)
+
+[M,N]=size(J);
+
+for k = 1:20
+    for i = randperm(M,M)
+        for j = randperm(N,N)
+            p = exp(E(i,j,1,linju(i,j,M,N,J),J))/(exp(E(i,j,1,linju(i,j,M,N,J),J))+exp(E(i,j,-1,linju(i,j,M,N,J),J)));
+            if p<rand(1)
+                J(i,j)=1;
+            else
+                J(i,j)=-1;
+            end
+        end
+    end
+    k
+end
+figure
+imshow(J)
+
 
 
 h=1;
 beta = 1;
 sigma = 1;
 [M,N]=size(J)
-
-
+T=J;
+J=T;
 for i = randperm(M,M)
     for j = randperm(N,N)
         if E(i,j,1,linju(i,j,M,N,J),J)<E(i,j,-1,linju(i,j,M,N,J),J)
@@ -25,13 +53,14 @@ for i = randperm(M,M)
             J(i,j)=-1;
         end
     end
+    imshow(J)
 end
 figure
 imshow(J)
 
 
 
-
+J=T;
 for i = 1:M
     for j = 1:N
         if E(i,j,1,linju(i,j,M,N,J),J)<E(i,j,-1,linju(i,j,M,N,J),J)
@@ -41,6 +70,8 @@ for i = 1:M
         end
     end
 end
+figure
+imshow(J)
 
 for i = 1 : 582
     for j =1:1087
